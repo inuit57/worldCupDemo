8b40630820efb34e5fc7d5c0e67b669b6981e8f2
inuit57 <inuit57@naver.com>
2022-08-08 13:10:45 +0900

    fix : 회원 생성 테스트 코드 수정

--- src/test/java/com/myWorldcup/demo/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/myWorldcup/demo/member/repository/MemberRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.myWorldcup.demo.member.repository;
 
 import com.myWorldcup.demo.member.domain.Member;
+import com.myWorldcup.demo.member.domain.MemberFactory;
+import com.myWorldcup.demo.member.domain.MemberForm;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,14 +19,17 @@ class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
+    MemberFactory memberFactory = new MemberFactory();
+
     @Test
     @DisplayName("유저 생성 테스트")
     @Transactional
     public void createMemberTest() {
         //given
-        Member member = new Member();
-        member.setUserId("test");
-        member.setNickname("testAA");
+
+        Member member = memberFactory.createMember(1L);
+//        member.setUserId("test");
+//        member.setNickname("testAA");
 
         //when
         Long savedId = memberRepository.save(member);
